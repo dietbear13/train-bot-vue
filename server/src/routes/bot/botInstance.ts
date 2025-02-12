e8cbe7a1be5bac3d83e71aa2d26a219d34e87a05
Mikhail Bulgakov <dietbear13@gmail.com>
2025-02-12 20:53:28 +0400

    Рефакторинг сервера: закончил и2, в и3 начнал рефакторинг маршрутов пользователей

--- server/src/routes/bot/botInstance.ts
+++ server/src/routes/bot/botInstance.ts
@@ -2,10 +2,12 @@
 
 import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
+import {config} from "../../config/env";
+
 dotenv.config();
 
 const botToken = process.env.TELEGRAM_BOT_API_KEY as string;
-export const bot = new TelegramBot(botToken, { polling: false });
+export const bot = new TelegramBot(config.botToken, { polling: true });
 
 // Важно: URL вашего мини-приложения (из .env)
 export const appUrl = process.env.APP_URL;
