4686bdbf2b23b9e8fa563a9ae45f9f3ed831cc6f
Mikhail Bulgakov <dietbear13@gmail.com>
2025-02-13 08:15:14 +0400

    убрал кэширование данных - оно не работало, нужно вернуться к кэшу перманентно

---
+++ eat-tg/plugins/pinia.ts
@@ -0,0 +1,10 @@
+// ~/plugins/pinia.ts
+import { defineNuxtPlugin } from '#app';
+import { createPinia } from 'pinia';
+import piniaPersist from 'pinia-plugin-persistedstate';
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const pinia = createPinia();
+    pinia.use(piniaPersist);
+    nuxtApp.vueApp.use(pinia);
+});
